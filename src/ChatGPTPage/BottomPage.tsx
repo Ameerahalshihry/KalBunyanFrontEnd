09430669c54c92e063c9ff0c919bf5f1e873d9ea
Riyan <Riyan@DESKTOP-9Q695GJ>
2023-03-12 22:07:13 +0300

    edited chatgpt page

---
+++ src/ChatGPTPage/BottomPage.tsx
@@ -0,0 +1,87 @@
+import { FormControl, Image, FormLabel, Input, Button, Box, Heading, Container, Stack } from '@chakra-ui/react'
+import Typewriter, { TypewriterClass } from 'typewriter-effect';
+
+import React, { useState } from 'react'
+import { wrap } from 'framer-motion';
+import Features from './Features';
+
+function BottomPage() {
+    const [prompt, setPrompt] = useState('')
+    const [response, setResponse] = useState('')
+    const [isTyping, setisTyping] = useState(false)
+
+    const handleSubmit = async ()=>{
+        setResponse('');
+        console.log(prompt)
+        const chatResponse = await fetch("http://localhost:3000/chat/prompt", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify({ prompt: prompt })
+        });
+        let res = await chatResponse.json();
+        setResponse(res.response);
+        console.log(response)
+    } 
+    
+    const arabic = /[\u0600-\u06FF]/;
+    if(response && arabic.test(response)){
+    document.documentElement.dir = "rtl"}
+    else{
+      document.documentElement.dir = "ltr"
+    }
+    return (
+      
+      <Box width="100% "   minHeight="100vh" display="flex" flexDirection="column"  alignItems="center">
+        <Box textAlign="center" py={4}>
+          <Heading>Life Coach Bot</Heading>
+        </Box>
+        <Box flex="1" display="flex" justifyContent="center" alignItems="center">
+          <Container maxW="80vw">
+            <Box mb={20}>
+              <Box maxWidth={"100%"} >
+                <code style={{ whiteSpace: "pre-line", fontSize:"1.5em", wordWrap: "break-word" }}>
+                  {response ? (
+                    <Typewriter
+                      options={{ delay: 25 }}
+                      onInit={(typewriter) => {
+                        typewriter.typeString(response).start();
+                      }}
+                    />
+                  ): <Features/>}
+                </code>
+              </Box>
+            </Box>
+          </Container>
+        </Box>
+        <Box minWidth={"100%"} pb={4}>
+          <Container   >
+            <Stack  direction="row" align="center">
+              <Input  size = "lg"
+               width="100%"
+               
+              bgColor={"white"}
+                placeholder="Type your message here"
+                flex="1"
+                type="text"
+                value={prompt}
+                onChange={(event) => setPrompt(event.target.value)}
+              />
+              <Button
+                bg="#103D3F"
+                onClick={handleSubmit}
+                ml={4}
+                colorScheme="orange"
+              >
+                Send
+              </Button>
+            </Stack>
+          </Container>
+        </Box>
+      </Box>
+    );
+    
+}
+
+export default BottomPage
---
+++ src/ChatGPTPage/Features.tsx
@@ -0,0 +1,62 @@
+import { ReactElement } from 'react';
+import { Box, SimpleGrid, Icon, Text, Stack, Flex } from '@chakra-ui/react';
+import { FcVoicePresentation, FcMindMap, FcLock } from 'react-icons/fc';
+
+interface FeatureProps {
+  title: string;
+  text: string;
+  icon: ReactElement;
+}
+
+const Feature = ({ title, text, icon }: FeatureProps) => {
+  return (
+    <Stack>
+      <Flex
+        w={16}
+        h={16}
+        align={'center'}
+        justify={'center'}
+        color={'white'}
+        rounded={'full'}
+        bg={'gray.100'}
+        mb={1}>
+        {icon}
+      </Flex>
+      <Text fontWeight={600}>{title}</Text>
+      <Text color={'gray.600'}>{text}</Text>
+    </Stack>
+  );
+};
+
+export default function SimpleThreeColumns() {
+    return (
+        <Box p={4}>
+          
+            <SimpleGrid columns={{ base: 1, md: 3 }} spacing={10}>
+              <Feature
+                icon={<Icon as={FcMindMap} w={10} h={10} />}
+                title={'Support'}
+                text={
+                  "You can ask it a wide variety of questions, as long as they're relevant to the bot's job"
+                }
+              />
+              <Feature
+                icon={<Icon as={FcLock} w={10} h={10} />}
+                title={'Confidentiality'}
+                text={
+                  'We take your privacy seriously and ensure that all conversations and personal information are kept confidential.'
+                }
+              />
+              <Feature
+                icon={<Icon as={FcVoicePresentation} w={10} h={10} />}
+                title={'Example'}
+                text={
+                  "'I'm struggling to balance my work and personal life. How can I create a better balance?'" 
+                               }
+              />
+            </SimpleGrid>
+         
+    
+        </Box>
+      );
+}
