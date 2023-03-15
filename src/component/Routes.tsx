2055421cc66f2da795c20f7d30e1e39d5d74c0e9
Fatemah <f.thenyan@gmail.com>
2023-03-15 14:36:48 +0300

    components updated

--- src/component/Routes.tsx
+++ src/component/Routes.tsx
@@ -4,14 +4,13 @@ import SignUp from './SignUp'
 import LogIn from './LogIn'
 import Home from './Home'
 import Sessions from './Sessions'
-import { CreateSession } from './CreateSession'
 import Chat from '../Chatroom/Chat'
 import ChatGPTPage from '../ChatGPTPage/ChatGPTpage'
 const Routes_ = () => {
   return (
     <Routes>
         <Route path='/' element={<Home />}></Route>
-        <Route path='/sessions' element={<CreateSession />}></Route>
+        <Route path='/sessions' element={<Sessions />}></Route>
         <Route path='/login' element={<LogIn />}></Route>
         <Route path='/signUp' element={<SignUp />}></Route>
         <Route path='/chat' element={<Chat sessionId='0f613b04-17a5-4fe4-b87d-3ca065be2cd7'/>}></Route>
--- src/component/sessionCard/SessionCardAcademic.tsx
+++ src/component/sessionCard/SessionCardAcademic.tsx
@@ -40,8 +40,7 @@ const SessionCardAcademic = () => {
                       </Flex>  
                     </CardHeader>
 
-                    <Stack bg='white' borderRadius={25} align='center'
-                    >
+                    <Stack bg='white' borderRadius={25} align='center' height={'full'} width={'full'}>
                     <CardBody textAlign='center' >
                       <Heading size='md'>{session.title}</Heading>
                       <Text py='2'>{session.discription}</Text>
@@ -90,7 +89,7 @@ const SessionCardAcademic = () => {
               </Flex>  
             </CardHeader>
             
-            <Stack bg='white' borderRadius={25} align='center' height={'full'}>
+            <Stack bg='white' borderRadius={25} align='center' height={'full'} width={'full'}>
               <CardBody textAlign='center' >
                 <Heading size='md'>هنا يمكنك طلب المساعدة، وإقتراح عنوان لجلسة تحدي جديدة ...</Heading>
                 <Input  mt={6} mb={0} variant='outline' boxShadow='md' focusBorderColor='#103D3F' placeholder='اكتب هنا' />
